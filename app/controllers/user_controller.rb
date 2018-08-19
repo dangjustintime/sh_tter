b9a0a71f07b34b0d08cfcc52f3b098bb0c059440
Justin Dang <dangjustintime@gmail.com>
2018-08-19 16:39:26 -0400

    added index route to controller

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,6 +1,18 @@
 class UsersController < ApplicationController
+  # connect Controller to postgreSQL database
+  DB = PG.connect(host: "localhost", port: 5432, dbname: "sh_tter_development");
   def index
-  end
+    results = DB.exec("SELECT * FROM users");
+    return results.map do |result|
+      {
+        "id" => result["id"].to_i,
+        "username" => result["username"],
+        "password" => result["password"],
+        "profilePic" => result["profilePic"],
+        "header" => result["header"],
+        "bio" => result["bio"]
+      }
+    end
   def show
   end
   def create

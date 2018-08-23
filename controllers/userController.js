1a81c26c0f8f7d66a0c70ff9fe300cb78ef44293
Justin Dang <dangjustintime@gmail.com>
2018-08-23 13:11:27 -0400

    added more routes to user controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -10,6 +10,42 @@ router.get("/", (request, response) => {
   });
 });
 
+// edit route
+router.put("/edit/:id", (request, response) => {
+  request.body.password = bcrypt.hashSync(request.body.password, bcrypt.genSaltSync(10));
+  User.findByIdAndUpdate(request.params.id, request.body,
+    (error, foundUser) => {
+      if (error) {
+        console.log(error);
+      }
+      response.json(foundUser);
+  });
+});
+
+// add follower route
+router.put("/addFollower/:id", (request, response) => {
+  User.findByIdAndUpdate(request.params.id,
+    { $push: { followers: request.body.follower } },
+    (error, foundUser) => {
+      if (error) {
+        console.log(error);
+      }
+      response.json(foundUser);
+    });
+});
+
+// following route
+router.put("/following/:id", (request, response) => {
+  User.findByIdAndUpdate(request.params.id,
+    { $push: { following: request.body.following } },
+    (error, foundUser) => {
+      if (error) {
+        console.log(error);
+      }
+      response.json(foundUser);
+    });
+});
+
 // show route
 router.get("/:id", (request, response) => {
   User.findById(request.params.id, (error, foundUser) => {
@@ -27,6 +63,16 @@ router.post('/', (request, response)=>{
   request.body.following = [];
   User.create(request.body, (err, createdUser)=>{
     response.status(201).json(createdUser)
+  });
+});
+
+// delete route
+router.delete("/:id", (request, response) => {
+  User.findByIdAndDelete(request.params.id, (error, deletedUser) => {
+    if (error) {
+      console.log(error);
+    }
+    response.json(deletedUser);
   });
 });
 
